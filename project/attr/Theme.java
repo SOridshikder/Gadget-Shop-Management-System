d409aefcfcec556e686aaef7b20db8ab6882b12a
Md.Sabbir Shikder Orid <oridshikder220@gmail.com>
2020-08-27 19:09:46 +0600

    first commit

---
+++ project/attr/Theme.java
@@ -0,0 +1,20 @@
+package attr;
+
+import java.awt.*;
+
+public class Theme {
+	public static final int GUI_WIDTH = 500;
+	public static final int GUI_HEIGHT = 500;
+	public static final int BUTTON_PRIMARY_WIDTH = 500;
+	
+	public static final Color BACKGROUND_PANEL = new Color(125, 125, 125);
+	public static final Color BACKGROUND_HEADER = new Color(0, 5, 50);
+	public static final Color COLOR_TITLE = new Color(0, 5, 53);
+	public static final Color COLOR_BUTTON_PRIMARY = new Color(125, 124, 125);
+	public static final Color BACKGROUND_BUTTON_PRIMARY = new Color(0, 5, 50);
+	
+	public static final Font FONT_TITLE = new Font(Font.SANS_SERIF, Font.BOLD, 20);
+	public static final Font FONT_BUTTON = new Font(Font.SANS_SERIF, Font.BOLD, 7);
+	public static final Font FONT_REGULAR = new Font(Font.SANS_SERIF, Font.PLAIN, 10);
+	public static final Font FONT_INPUT = new Font(Font.SANS_SERIF, Font.PLAIN, 8);
+}
